1167a5815af142e8634671fe2ffe145919f39725
jsimonas <simonas.juzenas@gmail.com>
2025-11-05 14:19:30 +0200

    update split bam by cb

--- preprocessing/scdnaseq_split_bam_based_on_cb.sh
+++ preprocessing/scdnaseq_split_bam_based_on_cb.sh
@@ -4,23 +4,28 @@
 species=(GRCh38 GRCm39)
 
 for sp in "${species[@]}"; do
+    
+    # get BAMs
+    bams=( $(find "$PWD" -type f -name "*.sortedByCoord.out.dedup_${sp}.bam") )
 
-  # get BAMs
-  bams=( $(find "$PWD" -type f -name "*.sortedByCoord.out.dedup_${sp}.bam") )
+    barcode_file="$PWD/qced_cell_barcodes_${sp}.txt"
+    N_CB=$(wc -l < "$barcode_file") 
+    
+    # split bam
+    for i in "${!bams[@]}"; do
+    
+        prefix="${bams[$i]%.*}"
+        out_dir="$(dirname "$prefix")/perCB_${sp}"
+        mkdir -p "$out_dir"
 
-  # split bam
-  for i in "${!bams[@]}"; do
-
-      prefix="${bams[$i]%.*}"
-      out_dir="$(dirname "$prefix")/perCB_${sp}"
-      mkdir -p "$out_dir"
-
-      echo "processing: $(basename "$prefix")"
-      echo "bam: ${bams[$i]}"
-
-      # get top CBs
-      samtools view -bh -D CB:<(head -n 100 "$PWD/qced_cell_barcodes_${sp}.txt") "${bams[$i]}" \
-        | samtools split -d CB -u /dev/null -f "${out_dir}/perCB_%!.bam" -
+        echo "processing: $(basename "$prefix")"
+        echo "bam: ${bams[$i]}"
+    
+        ulimit -n $(( N_CB > 1014 ? N_CB + 10 : 1024 ))
+      
+        # get qced CBs
+        samtools view -bh -D CB:<(cat "$barcode_file") "${bams[$i]}" \
+        | samtools split -d CB -M "$N_CB" -u /dev/null -f "${out_dir}/perCB_%!.bam" -
 
       echo 'done'
 
